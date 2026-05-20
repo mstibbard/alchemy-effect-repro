3e64cb36105b59330f1e25eba4b9ad8037baac07
Matthew Stibbard <matthew@stibbard.au>
2026-05-20 19:52:36 +1000

    refactor(api): extract task domain schemas

--- apps/api/src/task.ts
+++ apps/api/src/task.ts
@@ -1,22 +1,7 @@
 import type { KVNamespaceClient } from "alchemy/Cloudflare";
 import * as Effect from "effect/Effect";
-import * as Schema from "effect/Schema";
-
-export class Task extends Schema.Class<Task>("Task")({
-	id: Schema.String,
-	title: Schema.String,
-	completed: Schema.Boolean,
-}) {}
-
-export class TaskNotFound extends Schema.TaggedClass<TaskNotFound>()("TaskNotFound", { id: Schema.String }) {}
-
-export class CreateTaskFailed extends Schema.TaggedClass<CreateTaskFailed>()("CreateTaskFailed", {
-	message: Schema.String,
-}) {}
-
-export class ListTasksFailed extends Schema.TaggedClass<ListTasksFailed>()("ListTasksFailed", {
-	message: Schema.String,
-}) {}
+export { CreateTaskFailed, ListTasksFailed, Task, TaskNotFound } from "./domain/task.ts";
+import { CreateTaskFailed, ListTasksFailed, Task, TaskNotFound } from "./domain/task.ts";
 
 export const listTasks = (tasks: KVNamespaceClient<string>) =>
 	Effect.gen(function* () {
